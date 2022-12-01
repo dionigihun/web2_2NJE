0dc6307d0fb268eab2e9a5a8b9b7174719227918
oláhDénes <dionigi.hun@gmail.com>
2022-12-01 17:16:45 +0100

    second commit

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="container py-5">
     <div class="row">
         <div class="col-12 col-md">
-            <small class="d-block mb-3 text-muted">Készítette: Oláh Dénes és Földházi Balázs 2022</small>
+            <small class="d-block mb-3 text-muted">Készítette: Oláh Dénes és Földházi Balázs 2022<br>A weboldal 2022-ben készült a Web-programozás II. kurzusra</small>
         </div>
     </div>
 </footer>
