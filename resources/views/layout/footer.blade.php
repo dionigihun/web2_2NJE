df3ac6e520cc140d90824126a4821e16cbb9b335
oláhDénes <dionigi.hun@gmail.com>
2022-12-01 11:32:13 +0100

    first commmit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="container py-5">
+    <div class="row">
+        <div class="col-12 col-md">
+            <small class="d-block mb-3 text-muted">Készítette: Oláh Dénes és Földházi Balázs 2022</small>
+        </div>
+    </div>
+</footer>
